635d02604bacd4d26bb04a49bfe5e89831394567
GabeCris <gabrecrisanto@gmail.com>
2023-02-11 16:53:31 -0300

    feat: products page and shelf started

---
+++ src/components/ShelfProduct/ShelfProduct.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Button from "../Button";
+
+const ShelfProduct = () => {
+    const srcImage =
+        "https://www.havan.com.br/media/catalog/product/cache/b23e5304d11dff00750c3e92e4b038fa/t/o/toalha-de-rosto-toscana-havan_604489.jpg";
+    const name = "Toalha de rosto";
+    const price = 59.46;
+    
+    return (
+        <div className="shelfProduct">
+            <img src={srcImage} className="shelfProduct-image" />
+            <h2 className="shelfProduct-name">{name}</h2>
+            <p className="shelfProduct-price">R${price}</p>
+            <Button label="Ver no site" />
+        </div>
+    );
+};
+
+export default ShelfProduct;
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -2,17 +2,25 @@ import React from "react";
 import Layout from "../../components/Layout/Layout";
 import Filter from "../../components/Filter";
 import Button from "../../components/Button";
-import { useFilter } from "../../contexts/FilterContext/FilterContext";
+import ShelfProduct from "../../components/ShelfProduct";
 
 const Products = () => {
-    const {selected} = useFilter();
     return (
         <Layout>
             <Filter />
             <section className="products-container">
-                A categoria selecionada foi "<b>{selected.label}</b>"
-                <Button label={"Voltar"} url={"filter"}></Button>
+                <ShelfProduct />
+                <ShelfProduct />
+                <ShelfProduct />
+                <ShelfProduct />
+                <ShelfProduct />
+                <ShelfProduct />
+                <span className="shadow"></span>
             </section>
+            <p>
+                <b>Total:</b> 6 produtos encontrados
+            </p>
+            <Button label={"Voltar"} url={"filter"}></Button>
         </Layout>
     );
 };
